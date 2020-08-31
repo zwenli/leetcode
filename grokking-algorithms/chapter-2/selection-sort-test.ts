8ceb168d911ddfe877d32951878b5a409d00ea58
zhangwenli <glaswen9405@gmail.com>
2020-08-31 22:55:30 +0800

    733

--- grokking-algorithms/chapter-2/selection-sort-test.ts
+++
@@ -1,3 +0,0 @@
-import selectionSort from './selection-sort.ts';
-
-console.log(selectionSort([2, 6, 9, 8, 5, 1])); // 1,2,5,8,9
--- problems/733.图像渲染.js
+++ problems/733.图像渲染.js
@@ -12,7 +12,45 @@
  * @param {number} newColor
  * @return {number[][]}
  */
-const floodFill = function (image, sr, sc, newColor) {
-
+const floodFill = (image, sr, sc, newColor) => {
+  const xL = image.length;
+  const yL = image[0].length;
+  const newIamge = JSON.parse(JSON.stringify(image));
+  const queue = [[sr, sc]];
+  const set = new Set();
+  set.add([sr, sc].toString());
+  const color = image[sr][sc];
+  while (queue.length > 0) {
+    const point = queue.shift();
+    const [x, y] = point;
+    set.add(point.toString());
+    newIamge[x][y] = newColor;
+    // 上下左右
+    if (x + 1 < xL && newIamge[x + 1][y] === color && !set.has([x + 1, y].toString())) {
+      set.add([x + 1, y]);
+      queue.push([x + 1, y]);
+    }
+    if (x - 1 >= 0 && newIamge[x - 1][y] === color && !set.has([x - 1, y].toString())) {
+      set.add([x - 1, y]);
+      queue.push([x - 1, y]);
+    }
+    if (y + 1 < yL && newIamge[x][y + 1] === color && !set.has([x, y + 1].toString())) {
+      set.add([x, y + 1]);
+      queue.push([x, y + 1]);
+    }
+    if (y - 1 >= 0 && newIamge[x][y - 1] === color && !set.has([x, y - 1].toString())) {
+      set.add([x, y - 1]);
+      queue.push([x, y - 1]);
+    }
+  }
+  return newIamge;
 };
 // @lc code=end
+
+export {
+  floodFill as default,
+};
+
+// const res = floodFill([[1, 1, 1], [1, 1, 0], [1, 0, 1]], 1, 1, 2);
+
+// console.log(res);

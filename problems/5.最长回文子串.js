3f8979b52d4d0765df95b656b5b3946a4074e206
zhangwenli <glaswen9405@gmail.com>
2020-09-22 17:46:42 +0800

    [5] 最长回文子串：中心扩展算法, 基于动态规划

--- problems/5.最长回文子串.js
+++ problems/5.最长回文子串.js
@@ -5,32 +5,73 @@
  */
 
 // @lc code=start
+
+/**
+ * @param {*} s
+ * @param {*} left
+ * @param {*} right
+ * @return {number}
+ */
+function expandAroundCenter(s, left, right) {
+  let l = left;
+  let r = right;
+  while (l >= 0 && r < s.length && s.charAt(l) === s.charAt(r)) {
+    l -= 1;
+    r += 1;
+  }
+  return r - l - 1;
+}
+
 function longestPalindrome(s) {
-  // 动态规划，状态转移方程为：
-  // P(i, i) = ture
-  // P(i, i + 1) = Si === Si+1
-  // P(i, j) = P(i + 1, j - 1) && Si === Sj
+  // 中心扩展算法, 基于动态规划
+  // p(i,j) <= p(i + 1, j - 1) <= p(j + n, j - 1) <= 某一边界情况
+  // 某一边界情况指 p(i,i),p(i, i + 1)
+  if (!s || !s.length) {
+    return '';
+  }
   const n = s.length;
-  const dp = Array(n).fill([]).map(() => Array(n).fill(null));
-  let ans = '';
-  // 注意遍历的顺序
-  for (let l = 0; l < n; l += 1) {
-    for (let i = 0; i + l < n; i += 1) {
-      const j = i + l;
-      if (l === 0) {
-        dp[i][j] = true;
-      } else if (l === 1) {
-        dp[i][j] = s.charAt(i) === s.charAt(j);
-      } else {
-        dp[i][j] = dp[i + 1][j - 1] && (s.charAt(i) === s.charAt(j));
-      }
-      if (dp[i][j] && l + 1 > ans.length) {
-        ans = s.substr(i, l + 1);
-      }
+  let start = 0;
+  let end = 0;
+  for (let i = 0; i < n; i += 1) {
+    const len1 = expandAroundCenter(s, i, i);
+    const len2 = expandAroundCenter(s, i, i + 1);
+    const len = Math.max(len1, len2);
+    if (len > end - start) {
+      start = i - Math.floor((len - 1) / 2);
+      end = i + Math.floor(len / 2);
     }
   }
-  return ans;
+  return s.substring(start, end + 1);
 }
+// @lc code=end
+
+// function longestPalindrome(s) {
+//   // 动态规划，状态转移方程为：
+//   // P(i, i) = ture
+//   // P(i, i + 1) = Si === Si+1
+//   // P(i, j) = P(i + 1, j - 1) && Si === Sj
+//   // 时间复杂度O(n^2);空间复杂度O(n^2)
+//   const n = s.length;
+//   const dp = Array(n).fill([]).map(() => Array(n).fill(null));
+//   let ans = '';
+//   // 注意遍历的顺序
+//   for (let l = 0; l < n; l += 1) {
+//     for (let i = 0; i + l < n; i += 1) {
+//       const j = i + l;
+//       if (l === 0) {
+//         dp[i][j] = true;
+//       } else if (l === 1) {
+//         dp[i][j] = s.charAt(i) === s.charAt(j);
+//       } else {
+//         dp[i][j] = dp[i + 1][j - 1] && (s.charAt(i) === s.charAt(j));
+//       }
+//       if (dp[i][j] && l + 1 > ans.length) {
+//         ans = s.substr(i, l + 1);
+//       }
+//     }
+//   }
+//   return ans;
+// }
 
 // // 判断字符串是否为回文串
 // function isPalindrome(str) {
@@ -81,8 +122,6 @@ function longestPalindrome(s) {
 //   }
 //   return res;
 // }
-
-// @lc code=end
 
 const res1 = longestPalindrome('babad'); // 'aba', 'bab';
 const res2 = longestPalindrome('cbbd'); // 'bb';

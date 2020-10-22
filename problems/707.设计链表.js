5742dfee450588a7a504445c3d3ded6b7ed80835
zhangwenli <glaswen9405@gmail.com>
2020-10-22 11:40:26 +0800

    [707] 设计链表，双链表实现

--- problems/707.设计链表.js
+++ problems/707.设计链表.js
@@ -5,22 +5,30 @@
  */
 
 // @lc code=start
-class MyListNode {
-  // 单链表节点
+class DoublyListNode {
+  // 双链表节点
   constructor(val) {
     this.next = null;
+    this.prev = null;
     this.val = val;
   }
 }
 
 class MyLinkedList {
   // 单链表
+  /**
+   * Initialize your data structure here.
+   */
   constructor() {
-    this.head = new MyListNode(null);
+    this.head = new DoublyListNode(null);
   }
 
+  /**
+   * Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+   * @param {number} index
+   * @return {number}
+   */
   get(index) {
-    // 取首个节点
     let node = this.head.next;
     let j = 0;
     while (node !== null) {
@@ -33,124 +41,102 @@ class MyLinkedList {
     return -1;
   }
 
+  /**
+   * Add a node of value val before the first element of the linked list.
+   * After the insertion, the new node will be the first node of the linked list.
+   * @param {number} val
+   * @return {void}
+   */
   addAtHead(val) {
-    const curNode = new MyListNode(val);
     const nextNode = this.head.next;
-    this.head.next = curNode;
-    curNode.next = nextNode;
+    const currNode = new DoublyListNode(val);
+    currNode.next = nextNode;
+    currNode.prev = this.head;
+    this.head.next = currNode;
+    if (nextNode) {
+      nextNode.prev = currNode;
+    }
   }
 
+  /**
+   * Append a node of value val to the last element of the linked list.
+   * @param {number} val
+   * @return {void}
+   */
   addAtTail(val) {
-    const curNode = new MyListNode(val);
-    let preNode = this.head;
-    while (preNode.next !== null) {
-      preNode = preNode.next;
+    const tailNode = new DoublyListNode(val);
+    let prevNode = this.head;
+    while (prevNode.next) {
+      prevNode = prevNode.next;
     }
-    preNode.next = curNode;
+    prevNode.next = tailNode;
+    tailNode.prev = prevNode;
   }
 
+  /**
+   * Add a node of value val before the index-th node in the linked list.
+   * If index equals to the length of linked list,
+   * the node will be appended to the end of linked list.
+   * If index is greater than the length, the node will not be inserted.
+   * @param {number} index
+   * @param {number} val
+   * @return {void}
+   */
   addAtIndex(index, val) {
-    if (index <= 0) {
-      this.addAtHead(val);
-      return;
-    }
-    // 这里理解为找上一个元素
-    let j = 0;
-    let preNode = this.head.next;
-    while (preNode.next !== null && j < index - 1) {
-      preNode = preNode.next;
+    let prevNode = this.head;
+    let j = -1;
+    // 如果index不是大于0，直接不循环
+    while (index > 0 && j < index - 1 && prevNode.next) {
+      prevNode = prevNode.next;
       j += 1;
     }
-    if (j === index - 1) {
-      const curNode = new MyListNode(val);
-      const nextNode = preNode.next;
-      preNode.next = curNode;
-      curNode.next = nextNode;
+    // 两种情况：插入到head；index在链表的长度之内
+    if (index <= 0 || j === index - 1) {
+      const currNode = new DoublyListNode(val);
+      const nextNode = prevNode.next;
+      currNode.next = nextNode;
+      currNode.prev = prevNode;
+      prevNode.next = currNode;
+      if (nextNode) {
+        nextNode.prev = currNode;
+      }
     }
   }
 
+  /**
+   * Delete the index-th node in the linked list, if the index is valid.
+   * @param {number} index
+   * @return {void}
+   */
   deleteAtIndex(index) {
-    if (index < 0) return;
+    let currNode = this.head.next;
+    if (index < 0 || !currNode) return;
+
     let j = 0;
-    let preNode = this.head;
-    let curNode = this.head.next;
-    while (curNode.next !== null && j < index) {
-      preNode = curNode;
-      curNode = curNode.next;
+    while (currNode.next && j < index) {
+      currNode = currNode.next;
       j += 1;
     }
     if (j === index) {
-      preNode.next = curNode.next;
-      curNode.next = null;
+      const prevNode = currNode.prev;
+      const nextNode = currNode.next;
+      prevNode.next = nextNode;
+      if (nextNode) {
+        nextNode.prev = prevNode;
+      }
     }
   }
 
-  toString() {
-    const arr = [];
-    let curNode = this.head.next;
-    while (curNode) {
-      arr.push(curNode.val);
-      curNode = curNode.next;
-    }
-    return arr.join(' -> ');
-  }
+  // toString() {
+  //   const arr = [];
+  //   let curNode = this.head.next;
+  //   while (curNode) {
+  //     arr.push(curNode.val);
+  //     curNode = curNode.next;
+  //   }
+  //   return arr.join(' -> ');
+  // }
 }
-/**
- * Initialize your data structure here.
- */
-// var MyLinkedList = function() {
-
-// };
-
-/**
- * Get the value of the index-th node in the linked list. If the index is invalid, return -1.
- * @param {number} index
- * @return {number}
- */
-// MyLinkedList.prototype.get = function(index) {
-
-// };
-
-/**
- * Add a node of value val before the first element of the linked list.
- * After the insertion, the new node will be the first node of the linked list.
- * @param {number} val
- * @return {void}
- */
-// MyLinkedList.prototype.addAtHead = function(val) {
-
-// };
-
-/**
- * Append a node of value val to the last element of the linked list.
- * @param {number} val
- * @return {void}
- */
-// MyLinkedList.prototype.addAtTail = function(val) {
-
-// };
-
-/**
- * Add a node of value val before the index-th node in the linked list.
- * If index equals to the length of linked list,
- * the node will be appended to the end of linked list.
- * If index is greater than the length, the node will not be inserted.
- * @param {number} index
- * @param {number} val
- * @return {void}
- */
-// MyLinkedList.prototype.addAtIndex = function(index, val) {
-
-// };
-
-/**
- * Delete the index-th node in the linked list, if the index is valid.
- * @param {number} index
- * @return {void}
- */
-// MyLinkedList.prototype.deleteAtIndex = function (index) {
-
-// };
 
 /**
  * Your MyLinkedList object will be instantiated and called as such:
@@ -162,6 +148,130 @@ class MyLinkedList {
  * obj.deleteAtIndex(index)
  */
 // @lc code=end
+
+// class MyListNode {
+//   // 单链表节点
+//   constructor(val) {
+//     this.next = null;
+//     this.val = val;
+//   }
+// }
+
+// class MyLinkedList {
+//   // 单链表
+//   /**
+//    * Initialize your data structure here.
+//    */
+//   constructor() {
+//     this.head = new MyListNode(null);
+//   }
+
+//   /**
+//    * Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+//    * @param {number} index
+//    * @return {number}
+//    */
+//   get(index) {
+//     // 取首个节点
+//     let node = this.head.next;
+//     let j = 0;
+//     while (node !== null) {
+//       if (j === index) {
+//         return node.val;
+//       }
+//       j += 1;
+//       node = node.next;
+//     }
+//     return -1;
+//   }
+
+//   /**
+//    * Add a node of value val before the first element of the linked list.
+//    * After the insertion, the new node will be the first node of the linked list.
+//    * @param {number} val
+//    * @return {void}
+//    */
+//   addAtHead(val) {
+//     const curNode = new MyListNode(val);
+//     const nextNode = this.head.next;
+//     this.head.next = curNode;
+//     curNode.next = nextNode;
+//   }
+
+//   /**
+//    * Append a node of value val to the last element of the linked list.
+//    * @param {number} val
+//    * @return {void}
+//    */
+//   addAtTail(val) {
+//     const curNode = new MyListNode(val);
+//     let preNode = this.head;
+//     while (preNode.next !== null) {
+//       preNode = preNode.next;
+//     }
+//     preNode.next = curNode;
+//   }
+
+//   /**
+//    * Add a node of value val before the index-th node in the linked list.
+//    * If index equals to the length of linked list,
+//    * the node will be appended to the end of linked list.
+//    * If index is greater than the length, the node will not be inserted.
+//    * @param {number} index
+//    * @param {number} val
+//    * @return {void}
+//    */
+//   addAtIndex(index, val) {
+//     if (index <= 0) {
+//       this.addAtHead(val);
+//       return;
+//     }
+//     // 这里理解为找上一个元素
+//     let j = 0;
+//     let preNode = this.head.next;
+//     while (preNode.next !== null && j < index - 1) {
+//       preNode = preNode.next;
+//       j += 1;
+//     }
+//     if (j === index - 1) {
+//       const curNode = new MyListNode(val);
+//       const nextNode = preNode.next;
+//       preNode.next = curNode;
+//       curNode.next = nextNode;
+//     }
+//   }
+
+//   /**
+//    * Delete the index-th node in the linked list, if the index is valid.
+//    * @param {number} index
+//    * @return {void}
+//    */
+//   deleteAtIndex(index) {
+//     if (index < 0) return;
+//     let j = 0;
+//     let preNode = this.head;
+//     let curNode = this.head.next;
+//     while (curNode.next !== null && j < index) {
+//       preNode = curNode;
+//       curNode = curNode.next;
+//       j += 1;
+//     }
+//     if (j === index) {
+//       preNode.next = curNode.next;
+//       curNode.next = null;
+//     }
+//   }
+
+//   toString() {
+//     const arr = [];
+//     let curNode = this.head.next;
+//     while (curNode) {
+//       arr.push(curNode.val);
+//       curNode = curNode.next;
+//     }
+//     return arr.join(' -> ');
+//   }
+// }
 
 const linkedList = new MyLinkedList();
 linkedList.addAtHead(1);

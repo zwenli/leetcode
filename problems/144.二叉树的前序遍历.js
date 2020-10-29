4e19b5b54113415e9ba7d2e54e6c50953e17cd18
zhangwenli <glaswen9405@gmail.com>
2020-10-29 10:47:42 +0800

    [144] 二叉树的前序遍历：迭代

--- problems/144.二叉树的前序遍历.js
+++ problems/144.二叉树的前序遍历.js
@@ -20,27 +20,50 @@
  */
 function preorderTraversal(root) {
   // 迭代
+  // 迭代代的方式实现等价于递归，
+  // 区别在于递归的时候隐式地维护了一个栈，而我们在迭代的时候需要显式地将这个栈模拟出来
+
   const res = [];
+  if (!root) return res;
   const stack = [];
-  if (root) {
-    stack.push(root);
-  }
-  while (stack.length) {
-    const treeNode = stack.pop();
-    res.push(treeNode.val);
-    // 顺序上看起来和迭代的不一样，
-    // 其实stack为堆，先进先出，为了保证左节点先遍历，需要先把右节点压入栈再压左节点
-    if (treeNode.right) {
-      stack.push(treeNode.right);
+  let node = root;
+  while (stack.length || node) {
+    while (node) {
+      res.push(node.val);
+      // 压入当前节点，继续遍历左节点（因为当前节点还需要遍历右节点，保存起来用）
+      stack.push(node);
+      node = node.left;
     }
-    if (treeNode.left) {
-      stack.push(treeNode.left);
-    }
+    // 这里已说明左节点完成遍历，返回堆栈的顶部节点接着右节点遍历（遍历右节点时，说明节点已遍历完成，不需要再存在堆栈）
+    node = stack.pop();
+    node = node.right;
   }
   return res;
 }
 
 // @lc code=end
+
+// function preorderTraversal(root) {
+//   // 堆栈
+//   const res = [];
+//   const stack = [];
+//   if (root) {
+//     stack.push(root);
+//   }
+//   while (stack.length) {
+//     const treeNode = stack.pop();
+//     res.push(treeNode.val);
+//     // 顺序上看起来和迭代的不一样，
+//     // 其实stack为堆，先进先出，为了保证左节点先遍历，需要先把右节点压入栈再压左节点
+//     if (treeNode.right) {
+//       stack.push(treeNode.right);
+//     }
+//     if (treeNode.left) {
+//       stack.push(treeNode.left);
+//     }
+//   }
+//   return res;
+// }
 
 // function preorderTraversal(root) {
 //   // 递归

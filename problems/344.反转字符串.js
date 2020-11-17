f977654a61f1cb59de02666b5bbde3b0c6adf360
zhangwenli <glaswen9405@gmail.com>
2020-11-17 23:05:51 +0800

    [344] 反转字符串 递归

--- problems/344.反转字符串.js
+++ problems/344.反转字符串.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-shadow */
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-param-reassign */
 /*
  * @lc app=leetcode.cn id=344 lang=javascript
  *
@@ -9,18 +12,31 @@
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
-const reverseString = (s) => {
-  let i = 0;
-  let j = s.length - 1;
-  while (i < j) {
-    const temp = s[i];
-    s[i] = s[j];
-    s[j] = temp;
-    i += 1;
-    j -= 1;
+function reverseString(s) {
+  // 递归
+  if (!s || !s.length) return;
+  helper(s, 0, s.length - 1);
+  function helper(s, left, right) {
+    if (left >= right) return;
+    [s[left], s[right]] = [s[right], s[left]];
+    helper(s, left + 1, right - 1);
   }
-};
+}
 // @lc code=end
+
+// const reverseString = (s) => {
+//   // 双指针
+//   let i = 0;
+//   let j = s.length - 1;
+//   while (i < j) {
+//     // [s[i], s[j]] = [s[j], s[i]];
+//     const temp = s[i];
+//     s[i] = s[j];
+//     s[j] = temp;
+//     i += 1;
+//     j -= 1;
+//   }
+// };
 
 const res1 = ['h', 'e', 'l', 'l', 'o'];
 reverseString(res1); // ["o","l","l","e","h"]

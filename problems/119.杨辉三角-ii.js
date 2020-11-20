7f85076740ecfb0419e30695927485dd4b2de1e5
zhangwenli <glaswen9405@gmail.com>
2020-11-20 23:25:14 +0800

    [119] 杨辉三角 II 递归

--- problems/119.杨辉三角-ii.js
+++ problems/119.杨辉三角-ii.js
@@ -4,24 +4,44 @@
  * [119] 杨辉三角 II
  */
 
+// TODO:
+
 // @lc code=start
 /**
  * @param {number} rowIndex
  * @return {number[]}
  */
 function getRow(rowIndex) {
-  if (rowIndex < 0) return [];
-  let row = [1];
-  for (let i = 1; i <= rowIndex; i += 1) {
-    const nextRow = [1];
-    for (let j = 1; j < i; j += 1) {
-      nextRow[j] = row[j] + row[j - 1];
-    }
-    nextRow.push(1);
-    row = nextRow;
+  // 递归
+  if (rowIndex === 0) {
+    return [1];
   }
+  if (rowIndex === 1) {
+    return [1, 1];
+  }
+  const preRow = getRow(rowIndex - 1);
+  const row = [1];
+  for (let i = 1; i < rowIndex; i += 1) {
+    row[i] = preRow[i] + preRow[i - 1];
+  }
+  row.push(1);
   return row;
 }
 // @lc code=end
+
+// function getRow(rowIndex) {
+//   // 动态规划
+//   if (rowIndex < 0) return [];
+//   let row = [1];
+//   for (let i = 1; i <= rowIndex; i += 1) {
+//     const nextRow = [1];
+//     for (let j = 1; j < i; j += 1) {
+//       nextRow[j] = row[j] + row[j - 1];
+//     }
+//     nextRow.push(1);
+//     row = nextRow;
+//   }
+//   return row;
+// }
 
 const res1 = getRow(3); // [1,3,3,1]

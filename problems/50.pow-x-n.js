86f27e67d61db8a4be2896817c38e50f48f9a1c3
zhangwenli <glaswen9405@gmail.com>
2021-03-29 09:33:20 +0800

    pown

--- problems/50.pow-x-n.js
+++ problems/50.pow-x-n.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable no-use-before-define */
 /* eslint-disable no-shadow */
 /*
@@ -14,23 +15,89 @@
  * @param {number} n
  * @return {number}
  */
-function myPow(x, n) {
-  // 快速幂 + 递归
-  // x^n = x^(n/2 * 2);
-  // 即可以递归地先计算x^(n/2);
-  // 根据递归计算的结果，如果n为偶数x^n = y^2 = x^(n/2)，如果n为奇数则为x^n=x*y^2 = x * x^((n-1)/2)
-  // 递归的边界为 n = 0，任意数的 0 次方均为 1。
-  // 时间复杂度O(logn)：2分法，每次递归指数都会减少一半
-  // 空间复杂度O(logn)：即递归的层数
 
-  function quickMul(x, n) {
-    if (n === 0) return 1;
-    const y = quickMul(x, Math.floor(n / 2));
-    return n % 2 === 0 ? y * y : x * y * y;
+// function myPow(x, n) {
+//   // 快速幂递归
+//   // 时间复杂度O(logn):
+//   // 空间复杂度O(logn):
+//   if (n < 0) {
+//     x = 1 / x;
+//     n = -n;
+//   }
+//   return pow(x, n);
+
+//   function pow(x, n) {
+//     if (n === 0) {
+//       return 1;
+//     }
+//     const y = pow(x, Math.floor(n / 2));
+//     return n % 2 === 1
+//       ? x * y * y
+//       : y * y;
+//   }
+// }
+
+function myPow(x, n) {
+  // 快速幂迭代
+  // 参考https://leetcode-cn.com/problems/powx-n/solution/powx-n-by-leetcode-solution/
+  // 时间复杂度O(logn):
+  // 空间复杂度O(1):
+  if (n < 0) {
+    x = 1 / x;
+    n = -n;
   }
-  return n > 0 ? quickMul(x, n) : 1 / quickMul(x, -n);
+  return pow(x, n);
+
+  function pow(x, n) {
+    let ans = 1;
+    let xContribute = x;
+    while (n > 0) {
+      if (n % 2 === 1) {
+        ans *= xContribute;
+      }
+      xContribute *= xContribute;
+      n = Math.floor(n / 2);
+    }
+    return ans;
+  }
 }
+// function myPow(x, n) {
+//   // 暴力迭代，会超时
+//   // 时间复杂度O(n):
+//   // 空间复杂度O(1):
+//   if (n < 0) {
+//     x = 1 / x;
+//     n = -n;
+//   }
+//   return pow(x, n);
+
+//   function pow(x, n) {
+//     let ans = 1;
+//     while (n > 0) {
+//       ans *= x;
+//       n -= 1;
+//     }
+//     return ans;
+//   }
+// }
 // @lc code=end
+
+// function myPow(x, n) {
+//   // 快速幂 + 递归
+//   // x^n = x^(n/2 * 2);
+//   // 即可以递归地先计算x^(n/2);
+//   // 根据递归计算的结果，如果n为偶数x^n = y^2 = x^(n/2)，如果n为奇数则为x^n=x*y^2 = x * x^((n-1)/2)
+//   // 递归的边界为 n = 0，任意数的 0 次方均为 1。
+//   // 时间复杂度O(logn)：2分法，每次递归指数都会减少一半
+//   // 空间复杂度O(logn)：即递归的层数
+
+//   function quickMul(x, n) {
+//     if (n === 0) return 1;
+//     const y = quickMul(x, Math.floor(n / 2));
+//     return n % 2 === 0 ? y * y : x * y * y;
+//   }
+//   return n > 0 ? quickMul(x, n) : 1 / quickMul(x, -n);
+// }
 
 // function myPow(x, n) {
 //   // 递归
@@ -47,3 +114,9 @@ function myPow(x, n) {
 const res1 = myPow(2, 10); // 1024
 const res2 = myPow(2, -2); // 0.25
 const res3 = myPow(0.00001, 2147483647); //
+
+// 1. 暴力递归： f(x, n) = x * f(x, n - 1) f(0) = 1
+// 2. 暴力迭代：
+// 3. 快速幂递归： n % 2 === 1 ? f(n) = x * f(x, n / 2) * f(x, n / 2)
+//                           : f(n) = f(x, n / 2) * f(x, n / 2)
+// 4. 快速幂迭代

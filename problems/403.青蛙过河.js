6c46d9c3545dbe93b698a7ad506cef04c7b64fc7
zhangwenli <glaswen9405@gmail.com>
2021-06-05 15:37:47 +0800

    feat: 403, 410

--- problems/403.青蛙过河.js
+++ problems/403.青蛙过河.js
@@ -12,45 +12,66 @@
 
 function canCross(stones) {
   // 动态规划
-  // dp(i,k) 表示 青蛙能否到达石头i，且上一次跳跃距离为k的状态
-  // dp(i,k) = dp(j,k+1) || dp(j, k) || dp(j, k-1)
-  // 式中j表示上一个的石头编号j，且满足stones[i]-stones[j] = k
-  // 初始条件为dp(0,0)=true,当找到一个dp(n-1,k)为真时，就可以知道青蛙能到达终点了
-  // 对于石头i，枚举所有的j，那么上一次跳跃距离k=stones[i]-stones[j],
-  // 如果在第j个石头上，其上一次跳跃距离可以是k-1,k,k+1三个其中一个，
-  // 这样才能从j跳到i，那么只需检查三个状态其中一个为真即可
-  // 推论
-  // 1. 现在石头编号为i时，上一次跳跃距离必定满足k <= i
-  //    当青蛙位于石头0时，k = 0,之后每次跳跃，青蛙的石头编码至少增加1，
-  //    而每次跳跃的距离最多增加1。
-  //    当跳跃m次后，青蛙所处现在的石头编号i >= m, 上次跳跃距离 k <= m
-  //    所以 i >= k
-  //    这样我们就可以从后往前枚举j，当上一次跳跃距离k>j+1时，我们即可以停止跳跃，
-  //    因为在第j个石头上我们最多只能跳跃j+1的距离
-  // 2. 当第i个石子与第i-1个石子距离超过i时，青蛙必定无法到达终点
-  //    由结论1可知，当青蛙到达第 i-1 个石子时，它的「上一次跳跃距离」至多为 i-1，因此青蛙在第 1 个石子上最远只能跳出 i 的距离。
-  //    而距离第 i-1 个石子最近的石子即为第 i 个石子，它们的距离超过了青蛙当前能跳出的最远距离，因此青蛙无路可跳。
-  //    因此我们可以提前检查是否有相邻的石子不满足条件，如果有，我们可以提前返回 false.
+  // time complexity O(n^2)
+  // space complexity O(n^2)
   const n = stones.length;
-  const dp = new Array(n).fill(0).map(() => new Array(n).fill(0));
-  dp[0][0] = true;
+  // dp(i,k) 表示第i个石头可以是否跳跃k个单元
+  const dp = new Array(n).fill(0).map(() => new Array(n).fill(false));
+  dp[0][1] = true; // 第一步只能跳跃一个单元
   for (let i = 1; i < n; i += 1) {
-    if (stones[i] - stones[i-1] > i) return false;
-  }
-  for (let i = 1; i < n; i += 1) {
-    for (let j = i - 1; j >= 0; j -= 1) {
-      const k = stones[i] - stones[j];
-      if (k > j + 1) {
-        break;
-      }
-      dp[i][k] = dp[j][k-1] || dp[j][k] || dp[j][k+1];
-      if (i === n - 1 && dp[i][k]) {
-        return true;
-      }
+    for (let j = 0; j < i; j += 1) {
+      const gap = stones[i] - stones[j];
+      if (gap < 0 || gap > n || !dp[j][gap]) continue;
+      dp[i][gap] = true;
+      if (gap - 1 >= 0) dp[i][gap - 1] = true;
+      if (gap + 1 < n) dp[i][gap + 1] = true;
+      if (i === n - 1) return true;
     }
   }
   return false;
 }
+// function canCross(stones) {
+//   // 动态规划
+//   // dp(i,k) 表示 青蛙能否到达石头i，且上一次跳跃距离为k的状态
+//   // dp(i,k) = dp(j,k+1) || dp(j, k) || dp(j, k-1)
+//   // 式中j表示上一个的石头编号j，且满足stones[i]-stones[j] = k
+//   // 初始条件为dp(0,0)=true,当找到一个dp(n-1,k)为真时，就可以知道青蛙能到达终点了
+//   // 对于石头i，枚举所有的j，那么上一次跳跃距离k=stones[i]-stones[j],
+//   // 如果在第j个石头上，其上一次跳跃距离可以是k-1,k,k+1三个其中一个，
+//   // 这样才能从j跳到i，那么只需检查三个状态其中一个为真即可
+//   // 推论
+//   // 1. 现在石头编号为i时，上一次跳跃距离必定满足k <= i
+//   //    当青蛙位于石头0时，k = 0,之后每次跳跃，青蛙的石头编码至少增加1，
+//   //    而每次跳跃的距离最多增加1。
+//   //    当跳跃m次后，青蛙所处现在的石头编号i >= m, 上次跳跃距离 k <= m
+//   //    所以 i >= k
+//   //    这样我们就可以从后往前枚举j，当上一次跳跃距离k>j+1时，我们即可以停止跳跃，
+//   //    因为在第j个石头上我们最多只能跳跃j+1的距离
+//   // 2. 当第i个石子与第i-1个石子距离超过i时，青蛙必定无法到达终点
+//   //    由结论1可知，当青蛙到达第 i-1 个石子时，它的「上一次跳跃距离」至多为 i-1，因此青蛙在第 1 个石子上最远只能跳出 i 的距离。
+//   //    而距离第 i-1 个石子最近的石子即为第 i 个石子，它们的距离超过了青蛙当前能跳出的最远距离，因此青蛙无路可跳。
+//   //    因此我们可以提前检查是否有相邻的石子不满足条件，如果有，我们可以提前返回 false.
+//   const n = stones.length;
+//   const dp = new Array(n).fill(0).map(() => new Array(n).fill(0));
+//   dp[0][0] = true;
+//   for (let i = 1; i < n; i += 1) {
+//     if (stones[i] - stones[i - 1] > i) return false;
+//   }
+//   for (let i = 1; i < n; i += 1) {
+//     for (let j = i - 1; j >= 0; j -= 1) {
+//       const k = stones[i] - stones[j];
+//       if (k > j + 1) {
+//         break;
+//       }
+//       dp[i][k] = dp[j][k - 1] || dp[j][k] || dp[j][k + 1];
+//       if (i === n - 1 && dp[i][k]) {
+//         return true;
+//       }
+//     }
+//   }
+//   return false;
+// }
+
 // function canCross(stones) {
 //   // 记忆化搜索
 //   const n = stones.length;
@@ -134,11 +155,11 @@ function canCross(stones) {
 // }
 // @lc code=end
 
-const res1 = canCross([0,1,3,5,6,8,12,17]);
+const res1 = canCross([0, 1, 3, 5, 6, 8, 12, 17]);
 // true
-const res2 = canCross([0,1,2,3,4,8,9,11]);
+const res2 = canCross([0, 1, 2, 3, 4, 8, 9, 11]);
 // false
-const res3 = canCross([0,2]);
+const res3 = canCross([0, 2]);
 // false
-const res4 = canCross([0,1,3,6,7]);
+const res4 = canCross([0, 1, 3, 6, 7]);
 // false
---
+++ problems/410.分割数组的最大值.js
@@ -0,0 +1,108 @@
+/* eslint-disable no-bitwise */
+/* eslint-disable no-restricted-syntax */
+/*
+ * @lc app=leetcode.cn id=410 lang=javascript
+ *
+ * [410] 分割数组的最大值
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} m
+ * @return {number}
+ */
+
+function splitArray(nums, m) {
+  // 二分查找+贪心
+  // 本题中，我们注意到：当我们选定一个值x，我们可以线性地验证是否存在一种分割方案，
+  // 满足其最大分割子数组和不超过x。策略如下：
+  //   贪心地模拟分割的过程，从前到后遍历数组，用sum表示当前分割子数组的和，
+  //   cnt表示已经分割出的子数组的数量（包括当前子数组），
+  //   那么每当 sum 加上当前值超过了 x，我们就把当前取的值作为新的一段分割子数组的开头，
+  //   并将 cnt 加 1。遍历结束后验证是否 cnt 不超过 m。
+  // 二分查找，二分查找的上界为数组的最大值，下界为数组中所有元素的和。
+  // 可以通过二分查找，可以得到最小的最大分割和。
+  // 注意事项：如果某个 数组各自和的最大值 恰恰好使得分割数为 m ，
+  // 此时不能放弃搜索，因为我们要使得这个最大值 最小化，
+  // 此时还应该继续尝试缩小这个 数组各自和的最大值 ，
+  // 使得分割数超过 m ，超过 m 的最后一个使得分割数为 m 的 数组各自和的最大值
+  // 就是我们要找的 最小值。
+  // time complexity O(n * log(sum - max))
+  // space complexity O(1)
+  let sum = 0;
+  let max = 0;
+  for (const num of nums) {
+    sum += num;
+    max = Math.max(max, num);
+  }
+  let left = max;
+  let right = sum;
+  while (left <= right) {
+    const mid = (left + right) >> 1;
+    const splits = getSplits(nums, mid);
+    if (splits > m) {
+      // 分割太多，说明「子数组各自的和的最大值」太小，
+      // 此时需要将「子数组各自的和的最大值」调大
+      left = mid + 1;
+    } else {
+      // 其余情况，可以继续调小「子数组各自的和的最大值」
+      right = mid - 1;
+    }
+  }
+  return left;
+
+  // 获取满足不超过「子数组各自的和的最大值」的分割数
+  function getSplits(nums, max) {
+    let cnt = 1; // 最少分割一个
+    let sum = 0;
+    for (const num of nums) {
+      if (sum + num > max) {
+        cnt += 1;
+        sum = num;
+      } else {
+        sum += num;
+      }
+    }
+    return cnt;
+  }
+}
+
+// function splitArray(nums, m) {
+//   // dp[i,j], 表示前i个数字，分割为j段得到的最大连续子数组和的最小值。
+//   // 转移方程如下：
+//   // dp[i,j] =  min(max(dp[k][j-1], sub[i,k+1])), k = [0, i-1]
+//   // sub(i,j) 表示数组中下标区间(i,j)内数字的和
+//   // dp[0,0] = 0;
+//   const n = nums.length;
+//   const dp = new Array(n + 1).fill(0).map(
+//     () => new Array(m + 1).fill(Infinity),
+//   );
+//   const sub = new Array(n + 1).fill(0);
+//   for (let i = 1; i <= n; i += 1) {
+//     sub[i] = sub[i - 1] + nums[i - 1];
+//   }
+//   // base case
+//   dp[0][0] = 0;
+//   for (let i = 1; i <= n; i += 1) {
+//     for (let j = 1; j <= Math.min(i, m); j += 1) {
+//       for (let k = 0; k < i; k += 1) {
+//         dp[i][j] = Math.min(
+//           dp[i][j],
+//           Math.max(dp[k][j - 1], sub[i] - sub[k]),
+//         );
+//       }
+//     }
+//   }
+//   return dp[n][m];
+// }
+// @lc code=end
+
+const res1 = splitArray([7, 2, 5, 10, 8], 2);
+// 18
+const res2 = splitArray([1, 2, 3, 4, 5], 2);
+// 9
+const res3 = splitArray([1, 4, 4], 3);
+// 4
+
+// TODO：二分查找

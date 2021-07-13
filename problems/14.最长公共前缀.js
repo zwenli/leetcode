d57e71177a794f418b3a94600435a204225b5cc6
zhangwenli <glaswen9405@gmail.com>
2021-07-13 13:49:47 +0800

    14

--- problems/14.最长公共前缀.js
+++ problems/14.最长公共前缀.js
@@ -9,22 +9,68 @@
  * @param {string[]} strs
  * @return {string}
  */
-function longestCommonPrefix(strs) {
-  if (!strs || !strs.length) return '';
-  const count = strs.length;
-  const { length } = strs[0];
 
-  for (let i = 0; i < length; i += 1) {
-    const char = strs[0].charAt(i);
-    for (let j = 1; j < count; j += 1) {
-      if (i === strs[j].length || char !== strs[j][i]) {
-        return strs[0].substr(0, i);
-      }
-    }
+function longestCommonPrefix(strs) {
+  // 3. 分治
+  // time complexity O(mn): m表示字符串的平均长度，n为字符串的数量，通过主定理推算出来的
+  //  T(n) = 2 * T(n/2) + O(m), 其中T(n) = O(mn)
+  // space complexity O(mlogn): 递归层数取决与n，O(logn)，每层需要O(m)的空间返回存储结果
+  if (!strs || !strs.length) return '';
+  const n = strs.length;
+  return recurse(strs, 0, n - 1);
+  
+  function recurse(strs, start, end) {
+    if (start === end) return strs[start];
+    const mid = (start + end) >> 1;
+    const lcpLeft = recurse(strs, start, mid);
+    const lcpRight = recurse(strs, mid + 1, end);
+    return commonPrefix(lcpLeft, lcpRight);
   }
-  // 情况为：只有一个字符串；所有字符串匹配；第一个字符串为空串等。
-  return strs[0];
+  function commonPrefix(lcpLeft, lcpRight) {
+    const length = Math.min(lcpLeft.length, lcpRight.length);
+    let i = 0;
+    while (i < length && lcpLeft[i] === lcpRight[i]) i += 1;
+    return lcpLeft.substr(0, i);
+  }
 }
+
+// function longestCommonPrefix(strs) {
+//   // 2. 纵向扫描
+//   // time complexity O(mn): m表示字符串的平均长度，n为字符串的数量
+//   // space complexity O(1)
+//   if (!strs || !strs.length) return '';
+//   const count = strs.length;
+//   const { length } = strs[0];
+
+//   for (let i = 0; i < length; i += 1) {
+//     const char = strs[0].charAt(i);
+//     for (let j = 1; j < count; j += 1) {
+//       if (i === strs[j].length || char !== strs[j][i]) {
+//         return strs[0].substr(0, i);
+//       }
+//     }
+//   }
+//   // 情况为：只有一个字符串；所有字符串匹配；第一个字符串为空串等。
+//   return strs[0];
+// }
+
+// function longestCommonPrefix(strs) {
+//   // 1. 横向扫描
+//   // time complexity O(mn): m表示字符串的平均长度，n为字符串的数量
+//   // space complexity O(1)
+//   if (!strs || !strs.length) return '';
+//   const n = strs.length;
+//   let ans = strs[0];
+//   for (let i = 1; i < n; i += 1) {
+//     let j = 0;
+//     while (j < ans.length && j < strs[i].length && ans[j] === strs[i][j]) {
+//       j += 1;
+//     }
+//     ans = ans.substr(0, j);
+//     if (ans === '') return ans;
+//   }
+//   return ans;
+// }
 // @lc code=end
 
 const res1 = longestCommonPrefix(['flower', 'flow', 'flight']); // fl
@@ -32,3 +78,13 @@ const res2 = longestCommonPrefix(['dog', 'racecar', 'car']); // ''
 const res3 = longestCommonPrefix(['', '']); // ''
 console.log(res1);
 console.log(res2);
+
+/**
+https://leetcode-cn.com/problems/longest-common-prefix/solution/zui-chang-gong-gong-qian-zhui-by-leetcode-solution/
+1. 横向扫描，依次遍历字符串数组中的每个字符串，
+对于每个遍历到的字符串，更新最长公共前缀，
+当遍历完所有的字符串以后，即可得到字符串数组中的最长公共前缀。
+2. 纵向扫描
+3. 分治，和1思路一样，只不过采用分治
+4. 二分查找
+ */
--- problems/8.字符串转换整数-atoi.js
+++ problems/8.字符串转换整数-atoi.js
@@ -11,38 +11,88 @@
  */
 
 function myAtoi(s) {
+  const MAX_VALUE = 2 ** 31 - 1;
+  const MIN_VALUE = (-2) ** 31;
   let sign = 1;
   let total = 0;
   let i = 0;
+  // 去除前导空格
   while (i < s.length && s[i] === ' ') {
     i += 1;
   }
+  // 符号字符处理
   if (['-', '+'].includes(s[i])) {
     sign = s[i] === '-' ? -1 : 1;
     i += 1;
   }
-  while (i < s.length && s[i] !== ' ' && !isNaN(Number(s[i]))) {
-    total = total * 10 + Number(s[i]);
+  // 数字字符处理，遇到非数字字符停止转换。
+  while (i < s.length && s[i] !== ' ' && Number.isInteger(Number(s[i]))) {
+    const num = Number(s[i]);
+    // if (total * 10 > MAX_VALUE ||
+    //   total === Math.floor(MAX_VALUE / 10) && num > MAX_VALUE % 10
+    // ) return MAX_VALUE;
+    // if (total * 10 < MIN_VALUE ||
+    //   total === Math.ceil(MIN_VALUE / 10) && num > -(MIN_VALUE % 10)
+    // ) return MIN_VALUE;
+    // // 每一步都要把符号位乘上去
+    // total = total * 10 + sign * num;
+    // i += 1;
+    if (total * 10 > MAX_VALUE || total === Math.floor(MAX_VALUE / 10) && num > MAX_VALUE % 10) {
+      return sign === -1 ? MIN_VALUE : MAX_VALUE
+    }
+    total = total * 10 + num;
     i += 1;
   }
-  total *= sign;
-  if (total < (-2) ** 31) return (-2) ** 31;
-  if (total > 2 ** 31 - 1) return 2 ** 31 -1;
-  return total;
+  // return total;
+  return total * sign;
 }
+// function myAtoi(s) {
+//   const MAX_VALUE = 2 ** 31 - 1;
+//   const MIN_VALUE = (-2) ** 31;
+//   let sign = 1;
+//   let total = 0;
+//   let i = 0;
+//   // 去除前导空格
+//   while (i < s.length && s[i] === ' ') {
+//     i += 1;
+//   }
+//   // 符号字符处理
+//   if (['-', '+'].includes(s[i])) {
+//     sign = s[i] === '-' ? -1 : 1;
+//     i += 1;
+//   }
+//   // 数字字符处理，遇到非数字字符停止转换。
+//   while (i < s.length && s[i] !== ' ' && !isNaN(Number(s[i]))) {
+//     total = total * 10 + Number(s[i]);
+//     i += 1;
+//   }
+//   total *= sign;
+//   // 优化，可以在循环过程中就判断是否超出范围
+//   if (total < MIN_VALUE) return MIN_VALUE;
+//   if (total > MAX_VALUE) return MAX_VALUE;
+//   return total;
+// }
 // @lc code=end
+const assert = require('assert').strict;
 
 const res1 = myAtoi('42');
-// 42
+assert.equal(res1, 42);
 const res2 = myAtoi('    -42');
-// -42
+assert.equal(res2, -42);
 const res3 = myAtoi('4193 with words');
-// 4193
+assert.equal(res3, 4193);
 const res4 = myAtoi('words and 987');
-// 0
+assert.equal(res4, 0);
 const res5 = myAtoi('-91283472332');
-// -2147483648
+assert.equal(res5, -2147483648);
+const res6 = myAtoi('2147483648');
+assert.equal(res6, 2147483647);
+const res7 = myAtoi('-2147483649');
+assert.equal(res7, -2147483648);
 
 /**
+
+1. 普通遍历完成
+2. 用状态机实现
 
  */

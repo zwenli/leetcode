f1615a8b46951ed0b51c245cca87f456905825e6
Kevin <527887405@qq.com>
2023-02-16 11:13:54 +0800

    feat: 🎸 767 桶思想的解法
    ps: leetcode使用的datastructures-js版本为3，与5不兼容。所以这里仍使用自己实现

--- problems/767.重构字符串.js
+++ problems/767.重构字符串.js
@@ -3,7 +3,6 @@
  *
  * [767] 重构字符串
  */
-
 // @lc code=start
 class MaxPQ {
   constructor(compareTo = (a, b) => a - b) {
@@ -89,12 +88,57 @@ class MaxPQ {
     return k * 2 + 1
   }
 }
+// /**
+//  * @param {string} s
+//  * @return {string}
+//  */
+// var reorganizeString = function (s) {
+//   // 基于最大堆的贪心，每次取前两个最大的字母
+//   const length = s.length
+//   let maxCount = 0
+//   const counts = {}
+//   // 统计字母数量
+//   for (const c of s) {
+//     counts[c] = (counts[c] ?? 0) + 1
+//     maxCount = Math.max(maxCount, counts[c])
+//   }
+//   // 重复字母最大数量超过长度一半，必定会出现现相邻的字母相同的情况，
+//   // 故是无法重构的
+//   if ((length + 1) >> 1 < maxCount) return ''
+
+//   const pq = new MaxPQ((a, b) => a.count - b.count)
+//   Object.keys(counts).forEach((char) => {
+//     pq.insert({
+//       char,
+//       count: counts[char],
+//     })
+//   })
+//   let ans = []
+//   while (pq.size > 1) {
+//     const x = pq.delMax()
+//     const y = pq.delMax()
+//     ans.push(x.char + y.char)
+//     x.count -= 1
+//     y.count -= 1
+//     if (x.count > 0) pq.insert(x)
+//     if (y.count > 0) pq.insert(y)
+//   }
+//   if (pq.size) {
+//     ans.push(pq.delMax().char)
+//   }
+//   return ans.join('')
+// }
+
 /**
  * @param {string} s
  * @return {string}
  */
 var reorganizeString = function (s) {
-  // 基于最大堆的贪心，每次取前两个最大的字母
+  // 贪心 + 桶思想
+  // 1. 将相同的字母放入不同的桶中以保证其彼此不会相邻，因此桶的数目应等于字符串中最多的元素的数目
+  // 2. 按贪心策略，优先填充数目最多的元素，对于每一种元素，循环在不同桶中进行填充，
+  //    由于桶的个数等于字符串中最多的元素的数目，因此每个桶中不会出现相同的元素，填充完毕后将桶依次相连即为答案
+  // 3. 
   const length = s.length
   let maxCount = 0
   const counts = {}
@@ -103,10 +147,8 @@ var reorganizeString = function (s) {
     counts[c] = (counts[c] ?? 0) + 1
     maxCount = Math.max(maxCount, counts[c])
   }
-  // 重复字母最大数量超过长度一半，必定会出现现相邻的字母相同的情况，
-  // 故是无法重构的
   if ((length + 1) >> 1 < maxCount) return ''
-
+  const buckets = new Array(maxCount).fill('')
   const pq = new MaxPQ((a, b) => a.count - b.count)
   Object.keys(counts).forEach((char) => {
     pq.insert({
@@ -114,20 +156,18 @@ var reorganizeString = function (s) {
       count: counts[char],
     })
   })
-  let ans = []
-  while (pq.size > 1) {
-    const x = pq.delMax()
-    const y = pq.delMax()
-    ans.push(x.char + y.char)
-    x.count -= 1
-    y.count -= 1
-    if (x.count > 0) pq.insert(x)
-    if (y.count > 0) pq.insert(y)
+  let index = 0
+  while (pq.size) {
+    // 优先填充数目最多的元素
+    let { char, count } = pq.delMax()
+    while (count) {
+      buckets[index] += char
+      count -= 1
+      // 循环在不同桶中进行填充
+      index = (index + 1) % maxCount
+    }
   }
-  if (pq.size) {
-    ans.push(pq.delMax().char)
-  }
-  return ans.join('')
+  return buckets.join('')
 }
 // @lc code=end
 
@@ -139,5 +179,8 @@ assert.equal(res1, 'aba')
 const res2 = reorganizeString('aaab')
 assert.equal(res2, '')
 
-// const res3 = reorganizeString('aaabbc')
-// assert.equal(res3, 'ababac')
+const res3 = reorganizeString('aaabbc')
+assert.equal(res3, 'ababac')
+
+const res4 = reorganizeString('baaba')
+assert.equal(res4, 'ababa')

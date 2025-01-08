520d7e4eb9e65b6b00e4dd921d529a53c142e30a
zhangwenli <527887405@qq.com>
2025-01-08 10:59:36 +0800

    feat: type challenges

--- .typeChallenges/00009-medium-deep-readonly.ts
+++ .typeChallenges/00009-medium-deep-readonly.ts
@@ -63,6 +63,11 @@ type Expected2 = { readonly a: string } | { readonly b: number }
 //   ? T // base case
 //   : { readonly [K in keyof T]: DeepReadonly<T[K]> } // 递归
 
+// keyof T[K] extends never ? T[K] : DeepReadonly<T[K]}：这是一个条件类型（Conditional Type），用于判断 T[K] 是否是一个对象类型。
+// keyof T[K]：获取 T[K] 的所有键的联合类型。
+// extends never：如果 keyof T[K] 是 never 类型，说明 T[K] 没有键，即 T[K] 不是一个对象类型（例如，基本类型、函数等）.
+// ? T[K]：如果 T[K] 不是对象类型，则直接返回 T[K]，保持其类型不变.
+// : DeepReadonly<T[K]>：如果 T[K] 是对象类型，则递归地应用 DeepReadonly 类型，将其所有嵌套属性也转换为只读类型.
 type DeepReadonly<T> = {
   readonly [K in keyof T]: keyof T[K] extends never
     ? T[K]
